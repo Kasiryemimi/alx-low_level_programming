d01aa2649a827db36707105d4c777e9f0fd4ae0b
kasiryemimi <marianahkasirye@gmail.com>
2022-12-10 23:34:48 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'q')
+			continue;
+		else if (i == 'e')
+			continue;
+	}
+	putchar('\n');
+	return (0);
+}
